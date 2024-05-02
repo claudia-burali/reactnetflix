f6275fd62f1bdd28ebfee148f21d9799053ccc23
claudia-burali <161461840+claudia-burali@users.noreply.github.com>
2024-05-02 17:10:48 +0200

    Revert "mod"
    This reverts commit 62ee30885b442054ca08d878f04ab825690487f7.

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,23 @@
+
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NetflixNav from "./components/NetflixNav";
 import NetflixFot from './components/NetflixFot';
 import NetflixMain from './components/NetflixMain';
-import ProfilePage from './components/ProfilePage';
-import AccountPage from './components/AccountPage';
+/*import ProfilePage from './components/ProfilePage';
+import AccountPage from './components/AccountPage';*/
 
 function App() {
   return (
-      <div className="App bg-black">
-        <BrowserRouter>
-          <NetflixNav />
-          <Routes>
-            <Route path="/" element={<NetflixMain />} />
-            <Route path="/profile" element={<ProfilePage />} />
-            <Route path="/account" element={<AccountPage />} />
-          </Routes>
-          <NetflixFot />
-        </BrowserRouter>
-      </div>
+    <div className="App bg-black">
+      <NetflixNav />
+      <NetflixMain />
+      <NetflixFot />
+    </div>
   );
 }
 
 export default App;
 
+/*<ProfilePage />
+<AccountPage />*/
--- src/components/NetflixMain.jsx
+++ src/components/NetflixMain.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import { Container, Row, Col, Button, Dropdown } from 'react-bootstrap';
-import NetflixGallery from '../components/NetflixGallery';
+import { Container, Row, Col, Button, Dropdown } from "react-bootstrap";
+import { Component } from "react";
+import NetflixGallery from "../components/NetflixGallery";
 
-const NetflixMain = () => {
+
+class NetflixMain extends Component {
+ render() {
   return (
     <Container className="my-3">
       <Row className="text-center text-light text-lg-start d-flex align-items-center justify-content-between my-4">
@@ -25,42 +28,51 @@ const NetflixMain = () => {
           </div>
         </Col>
         <Col>
-          <div className="d-flex gap-1 justify-content-end">
+           <div className="d-flex gap-1 justify-content-end">
             <Button variant="outline-light">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                className="bi bi-text-left"
-                viewBox="0 0 16 16"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M2 12.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5m0-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5"
-                />
-              </svg>
-            </Button>
-            <Button variant="outline-light">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                className="bi bi-grid-fill"
-                viewBox="0 0 16 16"
-              >
-                <path d="M1 2.5A1.5 1.5 0 0 1 2.5 1h3A1.5 1.5 0 0 1 7 2.5v3A1.5 1.5 0 0 1 5.5 7h-3A1.5 1.5 0 0 1 1 5.5zm8 0A1.5 1.5 0 0 1 10.5 1h3A1.5 1.5 0 0 1 15 2.5v3A1.5 1.5 0 0 1 13.5 7h-3A1.5 1.5 0 0 1 9 5.5zm-8 8A1.5 1.5 0 0 1 2.5 9h3A1.5 1.5 0 0 1 7 10.5v3A1.5 1.5 0 0 1 5.5 15h-3A1.5 1.5 0 0 1 1 13.5zm8 0A1.5 1.5 0 0 1 10.5 9h3a1.5 1.5 0 0 1 1.5 1.5v3a1.5 1.5 0 0 1-1.5 1.5h-3A1.5 1.5 0 0 1 9 13.5z" />
-              </svg>
-            </Button>
-          </div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-text-left"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fillRule="evenodd"
+                d="M2 12.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5m0-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5"
+              />
+            </svg>
+          </Button>
+          <Button variant="outline-light">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-grid-fill"
+              viewBox="0 0 16 16"
+            >
+              <path d="M1 2.5A1.5 1.5 0 0 1 2.5 1h3A1.5 1.5 0 0 1 7 2.5v3A1.5 1.5 0 0 1 5.5 7h-3A1.5 1.5 0 0 1 1 5.5zm8 0A1.5 1.5 0 0 1 10.5 1h3A1.5 1.5 0 0 1 15 2.5v3A1.5 1.5 0 0 1 13.5 7h-3A1.5 1.5 0 0 1 9 5.5zm-8 8A1.5 1.5 0 0 1 2.5 9h3A1.5 1.5 0 0 1 7 10.5v3A1.5 1.5 0 0 1 5.5 15h-3A1.5 1.5 0 0 1 1 13.5zm8 0A1.5 1.5 0 0 1 10.5 9h3a1.5 1.5 0 0 1 1.5 1.5v3a1.5 1.5 0 0 1-1.5 1.5h-3A1.5 1.5 0 0 1 9 13.5z" />
+            </svg>
+          </Button></div>
+          
         </Col>
       </Row>
-      <NetflixGallery title={'Trending Now'} fetch={'Doctor Who'} />
-      <NetflixGallery title={'Watch it Again'} fetch={'Twilight'} />
-      <NetflixGallery title={'New Releases'} fetch={'Sherlock'} />
+      <NetflixGallery
+          title={"Trending Now"}
+          fetch={"Lord of the Rings"}
+        />
+        <NetflixGallery
+          title={"Watch it Again"}
+          fetch={"Harry Potter"}
+        />
+        <NetflixGallery
+          title={"New Releases"}
+          fetch={"Star Wars"}
+        />
     </Container>
   );
-};
+}}
 
 export default NetflixMain;
