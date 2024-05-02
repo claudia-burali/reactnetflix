62ee30885b442054ca08d878f04ab825690487f7
claudia-burali <161461840+claudia-burali@users.noreply.github.com>
2024-05-02 16:53:13 +0200

    mod

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
-
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NetflixNav from "./components/NetflixNav";
 import NetflixFot from './components/NetflixFot';
 import NetflixMain from './components/NetflixMain';
-/*import ProfilePage from './components/ProfilePage';
-import AccountPage from './components/AccountPage';*/
+import ProfilePage from './components/ProfilePage';
+import AccountPage from './components/AccountPage';
 
 function App() {
   return (
-    <div className="App bg-black">
-      <NetflixNav />
-      <NetflixMain />
-      <NetflixFot />
-    </div>
+      <div className="App bg-black">
+        <BrowserRouter>
+          <NetflixNav />
+          <Routes>
+            <Route path="/" element={<NetflixMain />} />
+            <Route path="/profile" element={<ProfilePage />} />
+            <Route path="/account" element={<AccountPage />} />
+          </Routes>
+          <NetflixFot />
+        </BrowserRouter>
+      </div>
   );
 }
 
 export default App;
 
-/*<ProfilePage />
-<AccountPage />*/
--- src/components/NetflixGallery.jsx
+++ src/components/NetflixGallery.jsx
@@ -1,108 +1,89 @@
-import React from 'react';
-import { Component } from "react";
-import { Alert, Col, Container, Spinner } from "react-bootstrap";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import Slider from "react-slick";
+import React, { useState, useEffect } from 'react';
+import { Alert, Col, Container, Spinner } from 'react-bootstrap';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import Slider from 'react-slick';
 
-class NetflixGallery extends Component {
-  state = {
-    Movies: [],
-    isLoading: false,
-    isError: false
-  };
+const NetflixGallery = (props) => {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
-  fetchMovies = () => {
-    this.setState({ isLoading: true });
+  const fetchMovies = () => {
+    setIsLoading(true);
 
-    console.log("Fetch in corso...");
-    fetch("http://www.omdbapi.com/?apikey=cec95536&s=" + this.props.fetch)
-
-      .then(response => {
+    console.log('Fetch in corso...');
+    fetch(`http://www.omdbapi.com/?apikey=cec95536&s=${props.fetch}`)
+      .then((response) => {
         if (response.ok) {
-          console.log("Fetch conclusa");
+          console.log('Fetch conclusa');
           return response.json();
         } else {
-          throw new Error("Errore nella richiesta");
+          throw new Error('Errore nella richiesta');
         }
       })
-
-      .then(Movies => {
-        console.log(Movies.Search);
-        this.setState({ Movies: Movies.Search })
-
+      .then((data) => {
+        console.log(data.Search);
+        setMovies(data.Search);
       })
-
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
-        this.setState({ isError: true });
+        setIsError(true);
       })
-      .finally(() => this.setState({ isLoading: false }));
+      .finally(() => setIsLoading(false));
   };
 
-  componentDidMount = (fetch) => {
-    this.fetchMovies();
+  useEffect(() => {
+    fetchMovies();
+  }, [props.fetch]);
+
+  const xMovies = movies.slice(0, 6);
+  const settings = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
 
-  render() {
-    const xMovies = this.state.Movies.filter((movie, index) => index < 6);
-    const settings = {
-        infinite: true,
-        speed: 500,
-        slidesToShow: 6,
-        slidesToScroll: 1,
-        responsive: [
-            {
-              breakpoint: 1024,
-              settings: {
-                slidesToShow: 3,
-                slidesToScroll: 1,
-              },
-            },
-            {
-              breakpoint: 600,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-              },
-            },
-            {
-              breakpoint: 480,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-              },
-            },
-          ],
-      };
-    return (
-      <Container>
-        <h3 className="text-white">
-            {this.props.title}
-            {this.state.isLoading && (
-                <Spinner animation="border" variant="danger" />
-            )}
-        </h3>
-        {this.state.isError && (
-            <Alert>Errore!</Alert>
-        )}
-        <Slider {...settings} className="mb-4">
-            {xMovies.map((movies) => {
-                return (
-                    <Col className="mb-2 text-center px-1" key={movies.imdbID}>
-                       <img
-                         src={movies.Poster}
-                         alt={movies.Title}
-                         className="img-card"
-                       />
-                   </Col>
-                )
-            }
-        )}
-        </Slider>
-      </Container>
-    );
-  }
-}
+  return (
+    <Container>
+      <h3 className="text-white">
+        {props.title}
+        {isLoading && <Spinner animation="border" variant="danger" />}
+      </h3>
+      {isError && <Alert>Errore!</Alert>}
+      <Slider {...settings} className="mb-4">
+        {xMovies.map((movie) => (
+          <Col className="mb-2 text-center px-1" key={movie.imdbID}>
+            <img src={movie.Poster} alt={movie.Title} className="img-card" />
+          </Col>
+        ))}
+      </Slider>
+    </Container>
+  );
+};
 
 export default NetflixGallery;
